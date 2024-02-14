64906bc55a68d44d156d461929bdc6350b25b5a3
Max <maxime.bossu@gmail.com>
2024-02-13 17:54:27 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from google.cloud import bigquery
+
+# Create a "Client" object
+client = bigquery.Client()
+
+# Construct a reference to the "chicago_crime" dataset
+dataset_ref = client.dataset("chicago_crime", project="bigquery-public-data")
+
+# API request - fetch the dataset
+dataset = client.get_dataset(dataset_ref)
